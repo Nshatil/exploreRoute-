6464365b139022b4e2816f13b0ba9820199fc536
Nshatil <shatil.mircl@gmail.com>
2024-10-03 13:36:34 +0600

    Basic ROuteing

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,48 @@ Route::get('/', function () {
 Route::get('/hello-shatil', function () {
     return view('hello-shatil');
 });
+
+
+ Route::get('/greeting', function () {
+    return 'Hello Route!!!';
+});
+
+// Route::get('/login', function () {
+//     return response("<h1>Hello </h1>",200,
+
+// ["Content-type" => "text/plain"]);
+// }); 
+
+// using any method
+
+Route::any('/greeting', function () {
+    return response(content: "<h1>HIIIIII</h1>", status:200)->header(key: "exampleheader", values: "abc");
+});
+
+// using Redirect method 
+
+Route::redirect('/here', '/there');
+
+Route::get('/there', function (){
+
+return "Redirected to THERE!!!!";
+
+
+});
+
+// View routes and passing values
+
+
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
+
+// Route Parameters
+
+Route::any('/delete/{user_id}', function ($user_id) {
+    return $user_id;
+})->where('user_id', '[0-9]+');
+
+// Named Route
+
+Route::get('/', function () {
+    return view('welcome');
+})->name("home");
